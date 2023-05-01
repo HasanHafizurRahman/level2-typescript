2aa6ca9d22c07ea7a2e34bac59f7c6ca343efd4c
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-01 17:57:32 +0600

    object function

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 const course = "Mezba vai milestone";
 console.log(course);
+// function
+function sum(num1, num2) {
+    return num1 - num2;
+}
+console.log(sum(1, 2));
+// make sqare of each value
+const array = [2, 3, 4];
+const result = array.map((sum) => sum * sum);
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 const course: string = "Mezba vai milestone";
 console.log(course);
+
+// function
+function sum(num1: number, num2: number) {
+  return num1 - num2;
+}
+console.log(sum(1, 2));
+
+// make sqare of each value
+const array = [2, 3, 4];
+
+const result = array.map((sum: number) => sum * sum);
+console.log(result);
+
+const Person: {
+  name: string;
+  salary: number;
+  age: number;
+  addMoney(money: number): number;
+} = {
+  name: "Hasan Hafizur Rahman",
+  salary: 25000,
+  age: 24,
+  addMoney(money: number) {
+    return this.salary + money;
+  },
+};
+console.log(Person.addMoney(29000));
