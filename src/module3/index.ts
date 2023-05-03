90ddc7c37280d58c8836e5d66642165e4c8e96c0
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-03 18:20:23 +0600

    generic types

--- src/module3/index.ts
+++ src/module3/index.ts
@@ -58,3 +58,41 @@ const customer: IExtendedUser = {
   age: 3000,
   role: "ibadah",
 };
+
+type numberTypeAlias = (num1: number, num2: number) => number;
+
+const addNumber: numberTypeAlias = (num1, num2) => num1 + num2; //type alias
+// or
+
+interface IAddnumber {
+  (num1: number, num2: number): number;
+}
+const addNumber1: IAddnumber = (num1, num2) => num1 + num2; //type interface
+
+// for array
+
+type rollnumberType = number[];
+const rollnumbers: rollnumberType = [1, 2, 3, 3, 5]; // type alias
+
+// or
+interface Irollnumber {
+  [index: number]: number;
+}
+const rollnumbers1: Irollnumber = [1, 2, 3, 3, 5]; // type interface
+
+// generic type
+
+// const roll : number[] = [1,2,3,4,5]  // OR,
+const roll: Array<number> = [1, 2, 3, 3, 5];
+
+// array of object
+const numberNameAndRoll: Array<{ name: string; roll: number }> = [
+  {
+    name: "jinn",
+    roll: 1,
+  },
+  {
+    name: "ferot",
+    roll: 2,
+  },
+];
